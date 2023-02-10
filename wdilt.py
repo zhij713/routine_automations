450ed2016a7f9e40fb3c34810dd833fdcf2db202
zhij713 <97318732+zhij713@users.noreply.github.com>
2023-02-10 13:37:19 -0800

    Added new catch-up functionality
    Sometimes, I would miss a day of doing WDILT review. New code (including a new method) provides structure for ability to catch up on previously missed reviews

--- wdilt.py
+++ wdilt.py
@@ -2,6 +2,7 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import json
 from datetime import date, timedelta
+import time
 
 class WDILT: #What Did I Learn Today: For recording daily learning nuggets and reviewing previous days' learnings for retention
     def __init__(self, cred=str, sheetName=str):
@@ -10,38 +11,55 @@ class WDILT: #What Did I Learn Today: For recording daily learning nuggets and r
         self.file = gspread.authorize(self.credentials) # authenticate the JSON key with gspread
         self.ws = self.file.open(sheetName).get_worksheet(0) #create variable for WDILT worksheet
 
-    def review(self,item,ctr=0):
+    def review(self,item,ctr=2):
         #Takes input of a cell containing review date
         #No output. Recursively indexes over and prints each learning nugget from the review date's row on the sheet
             wdilt_nugget = self.ws.cell(item.row,item.col + ctr).value
             if wdilt_nugget is None:
                 return
             print(wdilt_nugget+'\n')
+            self.ws.update_cell(item.row,2,"")
+            time.sleep(10) #Time Buffer to avoid read requests limit
             self.review(item,ctr+1)
     
-    def review_of_the_day(self,scale1=int,scale2=int):
-    #Inputs scalar parameters used to calculate review dates in terms of days back from today
+    def review_find(self, ctr=0):
+        #Function that recursively finds which rows correspond to the review dates, calling the review() function to print out the nuggets from each row
+        cell = self.ws.cell(1+ctr,1)
+        if cell.value is None: #Terminal condition, reaching last recorded row on worksheet
+            return
+        else:
+            if self.ws.cell(cell.row,2).value == 'X':
+                print(cell.value)
+                self.review(cell)
+            self.review_find(ctr+1)
+
+    
+    def review_of_the_day(self,scale1=int,scale2=int,date=date.today()):
+    #Inputs scalar parameters used to calculate review dates in terms of days back from specified date, which is by default today
     #No output. Determines review dates and prints their learning nuggets for review
         for x in range(1,6): #Up to 5 review dates are considered
             if x == 1:
-                ret_entry = date.today() - timedelta(1) #First review date is always from 1 day ago
+                ret_entry = date - timedelta(1) #First review date is always from 1 day ago
             elif x == 2:
                 ret_entry -= timedelta(scale1) 
+                scale1 *= scale2 #Increase retention interval for next review date''
             else:
                 ret_entry -= timedelta(scale1)
+                scale1 *= scale2 #Increase retention interval for next review date''
             ret_date = self.ws.find(str(ret_entry))
             if ret_date is not None:
-                self.review(ret_date)
+                self.ws.update_cell(ret_date.row,ret_date.col+1,"X")
 
-            scale1 *= scale2 #Increase retention interval for next review date''
+        self.review_find() 
             
-    def colindexer(self,cell, ctr=0):
+    
+
+    def colindexer(self,cell, ctr=2):
         #Inputs cell, ideally for today
         #Recursively indexes over cells within the same row and outputs the first found empty cell
         cell_value = self.ws.cell(cell.row,cell.col + ctr).value 
         if cell_value is None:
             next_cell = self.ws.cell(cell.row,cell.col+ctr)
-            print(next_cell)
             return next_cell
         else:
             return self.colindexer(cell,ctr+1)     
@@ -52,7 +70,7 @@ class WDILT: #What Did I Learn Today: For recording daily learning nuggets and r
         cell_value = self.ws.cell(cell.row+ctr,1).value 
         if cell_value is None:
             self.ws.update_cell(cell.row+ctr,1,str(date.today()))
-            return self.ws.cell(cell.row+ctr,2)
+            return self.ws.cell(cell.row+ctr,3)
         else:
             return self.rowindexer(cell,ctr+1)
 
@@ -85,10 +103,24 @@ def main():
 
     wdilt = WDILT(json_credentials,sheet_name)
 
-    askReview = input("Review time?")
+    askReview = input("Review time?  y/n")
     if askReview == 'y':
-        s1 = 5 #First scale setting for retention intervals
-        s2 = 3 #Second scale setting for retention intervals
+        s1 = 3 #First scale setting for retention intervals
+        s2 = 5 #Second scale setting for retention intervals
+        check_for_review_dates = [date.today()] #List of dates we want to do review on. By default just includes today, but will be appended if I missed any previous days 
+
+        askCatchup = input("Miss any previous sesions? y/n")
+
+        if askCatchup == 'y': #Case when I missed previous days of doing wdilt review and need to catch up on them
+            while True:
+                daysBack = input("How many days back (i.e., yesterday = 1)?")
+                if not daysBack: #Case for breaking loop, when there are no more missed catchup dates to input
+                    break
+                check_for_review_dates.append(date.today() - timedelta(int(daysBack)))
+
+        for item in check_for_review_dates:    
+            wdilt.review_of_the_day(s1,s2,item)
+
         wdilt.review_of_the_day(s1,s2)
         
     wdilt.nugget_recorder()
